27e35ca3df0aed1c1e301e98f9b75a6916518c02
Apclassic <brodeybriggs@hotmail.com>
2023-05-20 20:23:55 -0500

    Creating life itself (categories)

--- apps/backend/db/schema.rb
+++ apps/backend/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_14_014659) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_180026) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_id"
   end
 
 end
